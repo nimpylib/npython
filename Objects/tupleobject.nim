4694f65e5f33d455def86c0ca272c5a2e89d9f3d
litlighilit <litlighilit@foxmail.com>
2025-07-14 01:18:51 +0800

    feat(tuple): `__contains__`,index,count;feat(list): `__eq__`

--- Objects/tupleobject.nim
+++ Objects/tupleobject.nim
@@ -17,83 +17,123 @@ proc newPyTuple*(items: seq[PyObject]): PyTupleObject =
   result.items = items
 
 
-implTupleMagic eq:
-  if not other.ofPyTupleObject:
-    return pyFalseObj
-  let tOther = PyTupleObject(other)
-  if self.items.len != tOther.items.len:
-    return pyFalseObj
-  for i in 0..<self.items.len:
-    let i1 = self.items[i]
-    let i2 = tOther.items[i]
-    let retObj = i1.callMagic(eq, i2)
-    if retObj.isThrownException:
-      return retObj
-    assert retObj.ofPyBoolObject
-    if not PyBoolObject(retObj).b:
+template genSequenceMagics*(nameStr,
+    implNameMagic, implNameMethod;
+    ofPyNameObject, PyNameObject,
+    newPyNameSimple; mutRead, mutReadRepr;
+    reprs_left, reprs_right: char): untyped{.dirty.} =
+
+
+  implNameMagic eq, mutRead:
+    if not other.ofPyNameObject:
       return pyFalseObj
-  pyTrueObj
+    let tOther = PyNameObject(other)
+    if self.items.len != tOther.items.len:
+      return pyFalseObj
+    for i in 0..<self.items.len:
+      let i1 = self.items[i]
+      let i2 = tOther.items[i]
+      let retObj = i1.callMagic(eq, i2)
+      if retObj.isThrownException:
+        return retObj
+      assert retObj.ofPyBoolObject
+      if not PyBoolObject(retObj).b:
+        return pyFalseObj
+    pyTrueObj
+
+  implNameMagic contains, mutRead:
+    for idx, item in self.items:
+      let retObj =  item.callMagic(eq, other)
+      if retObj.isThrownException:
+        return retObj
+      if retObj == pyTrueObj:
+        return pyTrueObj
+    return pyFalseObj
 
 
-implTupleMagic iter: 
-  newPySeqIter(self.items)
+  implNameMagic iter, mutRead: 
+    newPySeqIter(self.items)
 
 
-implTupleMagic repr, [reprLock]:
-  var ss: seq[string]
-  for item in self.items:
-    var itemRepr: PyStrObject
-    let retObj = item.callMagic(repr)
-    errorIfNotString(retObj, "__repr__")
-    itemRepr = PyStrObject(retObj)
-    ss.add(itemRepr.str)
-  return newPyString("(" & ss.join(", ") & ")")
+  implNameMagic repr, mutReadRepr:
+    var ss: seq[string]
+    for item in self.items:
+      var itemRepr: PyStrObject
+      let retObj = item.callMagic(repr)
+      errorIfNotString(retObj, "__repr__")
+      itemRepr = PyStrObject(retObj)
+      ss.add(itemRepr.str)
+    return newPyString(reprs_left & ss.join(", ") & reprs_right)
 
+  implNameMagic len, mutRead:
+    newPyInt(self.items.len)
 
-implTupleMagic len:
-  newPyInt(self.items.len)
+  implNameMagic init:
+    if 1 < args.len:
+      let msg = nameStr & fmt" expected at most 1 args, got {args.len}"
+      return newTypeError(msg)
+    if self.items.len != 0:
+      self.items.setLen(0)
+    if args.len == 1:
+      let (iterable, nextMethod) = getIterableWithCheck(args[0])
+      if iterable.isThrownException:
+        return iterable
+      while true:
+        let nextObj = nextMethod(iterable)
+        if nextObj.isStopIter:
+          break
+        if nextObj.isThrownException:
+          return nextObj
+        self.items.add nextObj
+    pyNone
+
+  implNameMagic getitem:
+    if other.ofPyIntObject:
+      let idx = getIndex(PyIntObject(other), self.items.len)
+      return self.items[idx]
+    if other.ofPySliceObject:
+      let slice = PySliceObject(other)
+      let newObj = newPyNameSimple()
+      let retObj = slice.getSliceItems(self.items.addr, newObj.items.addr)
+      if retObj.isThrownException:
+        return retObj
+      else:
+        return newObj
+      
+    return newIndexTypeError(nameStr, other)
+
+  implNameMethod index(target: PyObject), mutRead:
+    for idx, item in self.items:
+      let retObj =  item.callMagic(eq, target)
+      if retObj.isThrownException:
+        return retObj
+      if retObj == pyTrueObj:
+        return newPyInt(idx)
+    let msg = fmt"{target} is not in " & nameStr
+    newValueError(msg)
+
+  implNameMethod count(target: PyObject), mutRead:
+    var count: int
+    for item in self.items:
+      let retObj = item.callMagic(eq, target)
+      if retObj.isThrownException:
+        return retObj
+      if retObj == pyTrueObj:
+        inc count
+    newPyInt(count)
+
+genSequenceMagics "tuple",
+  implTupleMagic, implTupleMethod,
+  ofPyTupleObject, PyTupleObject,
+  newPyTupleSimple, [], [reprLock],
+  '(', ')'
+
 
 implTupleMagic hash:
   var h = self.id
   for item in self.items:
     h = h xor item.id
   return newPyInt(h)
-
-
-implTupleMagic getitem:
-  if other.ofPyIntObject:
-    let idx = getIndex(PyIntObject(other), self.items.len)
-    return self.items[idx]
-  if other.ofPySliceObject:
-    let slice = PySliceObject(other)
-    let newList = newPyTupleSimple()
-    let retObj = slice.getSliceItems(self.items.addr, newList.items.addr)
-    if retObj.isThrownException:
-      return retObj
-    else:
-      return newList
-    
-  return newIndexTypeError("tuple", other)
-
-
-implTupleMagic init:
-  if 1 < args.len:
-    let msg = fmt"tuple expected at most 1 args, got {args.len}"
-    return newTypeError(msg)
-  if self.items.len != 0:
-    self.items.setLen(0)
-  if args.len == 1:
-    let (iterable, nextMethod) = getIterableWithCheck(args[0])
-    if iterable.isThrownException:
-      return iterable
-    while true:
-      let nextObj = nextMethod(iterable)
-      if nextObj.isStopIter:
-        break
-      if nextObj.isThrownException:
-        return nextObj
-      self.items.add nextObj
-  pyNone
 
 
 proc len*(t: PyTupleObject): int {. cdecl inline .} = 
