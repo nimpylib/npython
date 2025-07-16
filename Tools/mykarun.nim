9bbec5ac9ec0540fe30df5eccd66f71b2c1caa11
litlighilit <litlighilit@foxmail.com>
2025-07-17 00:39:39 +0800

    feat(mykarun): --includeJs for one-file html

--- Tools/mykarun.nim
+++ Tools/mykarun.nim
@@ -27,9 +27,26 @@ proc exec(cmd: string) =
   if os.execShellCmd(cmd) != 0:
     quit "External command failed: " & cmd
 
+const Buf = 1024
+proc extend(outf, inf: File) =
+  var buf: array[Buf, uint8]
+  while true:
+    let n = inf.readBytes(buf, 0, Buf)
+    if n == 0: break
+    let wn = outf.writeBytes(buf, 0, n)
+    assert n == wn
+proc extendNJsFileAndCompress(outf, inf: File) =
+  var line: string
+  while true:
+    line = inf.readLine
+    outf.writeLine line.strip(true, false, {' '})  # we know Nim current only use spaces for indentation
+    if inf.endOfFile:
+      break
+
 proc build(ssr: bool, entry: string, rest: string, selectedCss: string, run: bool,
     watch: bool,
-    jsDir, appName: string, htmlName=appName
+    jsDir, appName: string, htmlName=appName, includeJs = false,
+    release=false,
     ) =
   echo("Building...")
   var cmd: string
@@ -48,10 +65,30 @@ proc build(ssr: bool, entry: string, rest: string, selectedCss: string, run: boo
   else:
     exec cmd
   let dest = htmlName & ".html"
-  let script = if ssr:"" else: &"""<script type="text/javascript" src="{jsFp}"></script>""" # & (if watch: websocket else: "")
   if ssr: 
     content = readFile(outTempPath)
-  writeFile(dest, html % [if ssr: outHtmlName else:appName, selectedCss,content, script])
+  if includeJs:
+    defer: removeFile jsFp
+    assert not ssr, "includeJs and ssr are exclusive"
+    let parts = html.split("$4", 1)
+    assert parts.len == 2
+    var outF = open(dest, fmWrite)
+    outF.write parts[0] % [appName, selectedCss, ""]
+    outF.write"""<script type="text/javascript">"""
+    outF.write '\n'
+    let inF = open(jsFp)
+    if release:
+      outF.extendNJsFileAndCompress inf
+    else:
+      outF.extend inF
+    inF.close()
+    outF.write '\n'
+    outF.write"""</script>"""
+    outF.write parts[1]
+    outF.close()
+  else:
+    let script = if ssr:"" else: &"""<script type="text/javascript" src="{jsFp}"></script>""" # & (if watch: websocket else: "")
+    writeFile(dest, html % [if ssr: outHtmlName else:appName, selectedCss,content, script])
   if run: openDefaultBrowser("http://localhost:8080")
 
 proc main =
@@ -62,6 +99,7 @@ proc main =
     appName = "app"
     htmlName = appName
     htmlNameGiven = false
+    includeJs = false
   var file = ""
   var run = false
   var watch = false
@@ -86,6 +124,8 @@ proc main =
         appName = op.val
       of "jsDir":
         jsDir = op.val
+      of "includeJs":
+        includeJs = true
       of "run":
         run = true
       of "css":
@@ -123,6 +163,8 @@ proc main =
     htmlName = appName
   build(ssr, file, rest, selectedCss, run, watch,
     jsDir, appName, htmlName,
+    includeJs,
+    "-d:release" in rest  # XXX: not exhaustive
     )
   # sync()
 
