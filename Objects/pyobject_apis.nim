075b391e7de406e3d0624fa19005bc05bbe5a3eb
litlighilit <litlighilit@foxmail.com>
2025-09-07 01:14:06 +0800

    refact(pyobject_apis): split to ./strings; fixup: PyObject_Repr/Str...
    ... always raises TypeError for any exception from `__str__` or
    `__repr__`

--- Objects/pyobject_apis.nim
+++ Objects/pyobject_apis.nim
@@ -1,31 +1,4 @@
 
-import std/strformat
-import ./[pyobject, stringobject, exceptions]
-
-import ./pyobject_apis/[
-  attrs
-]
-export attrs
-
-proc reprDefault*(self: PyObject): PyObject {. cdecl .} = 
-  newPyString(fmt"<{self.typeName} object at {self.idStr}>")
-proc PyObject_ReprNonNil*(obj: PyObject): PyObject =
-  let fun = obj.getMagic(repr)
-  if fun.isNil:
-    return reprDefault obj
-  result = fun(obj)
-  result.errorIfNotString "__repr__"
-
-template nullOr(obj; elseCall): PyObject =
-  if obj.isNil: newPyAscii"<NULL>"
-  else: elseCall obj
-
-proc PyObject_Repr*(obj: PyObject): PyObject = obj.nullOr PyObject_ReprNonNil
-
-proc PyObject_StrNonNil*(obj: PyObject): PyObject =
-  let fun = obj.getMagic(str)
-  if fun.isNil: return PyObject_ReprNonNil(obj)
-  result = fun(obj)
-  result.errorIfNotString "__str__"
-
-proc PyObject_Str*(obj: PyObject): PyObject = obj.nullOr PyObject_StrNonNil
+import ../Utils/trans_imp
+impExp pyobject_apis,
+  attrs, strings
---
+++ Objects/pyobject_apis/strings.nim
@@ -0,0 +1,30 @@
+
+import std/strformat
+import ../[pyobject,
+  stringobject, exceptions,
+]
+
+proc reprDefault*(self: PyObject): PyObject {. cdecl .} = 
+  newPyString(fmt"<{self.typeName} object at {self.idStr}>")
+proc PyObject_ReprNonNil*(obj: PyObject): PyObject =
+  let fun = obj.getMagic(repr)
+  if fun.isNil:
+    return reprDefault obj
+  result = fun(obj)
+  retIfExc result
+  result.errorIfNotString "__repr__"
+
+template nullOr(obj; elseCall): PyObject =
+  if obj.isNil: newPyAscii"<NULL>"
+  else: elseCall obj
+
+proc PyObject_Repr*(obj: PyObject): PyObject = obj.nullOr PyObject_ReprNonNil
+
+proc PyObject_StrNonNil*(obj: PyObject): PyObject =
+  let fun = obj.getMagic(str)
+  if fun.isNil: return PyObject_ReprNonNil(obj)
+  result = fun(obj)
+  retIfExc result
+  result.errorIfNotString "__str__"
+
+proc PyObject_Str*(obj: PyObject): PyObject = obj.nullOr PyObject_StrNonNil
