6feefbb14037501ede71584bcbcf9e36097e5304
litlighilit <litlighilit@foxmail.com>
2025-09-10 10:42:17 +0800

    feat(js): import works when js

--- Python/pyimport.nim
+++ Python/pyimport.nim
@@ -17,11 +17,11 @@ let
   mnamekey = newPyAscii"__name__"
   mpathkey = newPyAscii"__file__"
 
-when not defined(js):
-  import std/os
-  proc init(m: PyModuleObject, filepath: string) =
-    let d = m.getDict
-    d[mpathkey] = newPyStr filepath
+proc init(m: PyModuleObject, filepath: string) =
+  let d = m.getDict
+  d[mpathkey] = newPyStr filepath
+import ../Utils/compat_io_os
+import std/os
 
 type Evaluator = object
   evalFrame: proc (f: PyFrameObject): PyObject {.raises: [].}
@@ -32,45 +32,43 @@ proc pyImport*(rt: Evaluator; name: PyStrObject): PyObject{.raises: [].} =
   if not modu.isNil:
     return modu
 
-  when defined(js):
-    newRunTimeError(newPyAscii"Can't import in js mode")
-  else:
-    var filepath: string
-    let sname = $name
-    for path in sys.path:
-      let p = joinPath($path, sname).addFileExt("py")
-      if p.fileExists:
-        filepath = p
-    
-    if filepath == "":
-      let msg = newPyStr&"No module named {name:R}"
-      retIfExc msg
-      let exc = newModuleNotFoundError(PyStrObject msg)
-      exc.name = name
-      exc.msg = msg
-      return exc
-    let input = try:
-      readFile(filepath)
-    except IOError as e:
-      #TODO:io maybe newIOError?
-      return newImportError(newPyAscii"Import Failed due to IOError " & newPyAscii $e.msg)
-    let compileRes = compile(input, filepath)
-    if compileRes.isThrownException:
-      return compileRes
+  var filepath: string
+  let sname = $name
+  for path in sys.path:
+    let p = joinPath($path, sname).addFileExt("py")
+    if p.fileExistsCompat:
+      filepath = p
+  
+  if filepath == "":
+    let msg = newPyStr&"No module named {name:R}"
+    retIfExc msg
+    let exc = newModuleNotFoundError(PyStrObject msg)
+    exc.name = name
+    exc.msg = msg
+    return exc
+  let input = try:
+    readFileCompat(filepath)
+  except IOError as e:
+    #TODO:io maybe newIOError?
+    return newImportError(newPyAscii"Import Failed due to IOError " & newPyAscii $e.msg)
+  let compileRes = compile(input, filepath)
+  if compileRes.isThrownException:
+    return compileRes
 
-    let co = PyCodeObject(compileRes)
+  let co = PyCodeObject(compileRes)
 
-    when defined(debug):
-      echo co
-    let fun = newPyFunc(name, co, newPyDict())
-    let f = newPyFrame(fun)
-    f.globals[mnamekey] = name
-    let retObj = rt.evalFrame(f)
-    if retObj.isThrownException:
-      return retObj
-    let module = newPyModule(name)
-    module.dict = f.globals
-    module.init filepath
-    let ret = sys.modules.setItem(name, module)
-    assert ret.isNil
-    module
+  when defined(debug):
+    echo co
+  let fun = newPyFunc(name, co, newPyDict())
+  let f = newPyFrame(fun)
+  # __name__ = '__main__'
+  f.globals[mnamekey] = name
+  let retObj = rt.evalFrame(f)
+  if retObj.isThrownException:
+    return retObj
+  let module = newPyModule(name)
+  module.dict = f.globals
+  module.init filepath
+  let ret = sys.modules.setItem(name, module)
+  assert ret.isNil
+  module
