f2c57d3708658dd1ebba49d64c9c3b53d5772a88
litlighilit <litlighilit@foxmail.com>
2025-09-10 19:13:46 +0800

    fix(py): PyObject_GenericGetAttrWithDict did not look on parents

--- Objects/pyobject_apis/attrsGeneric.nim
+++ Objects/pyobject_apis/attrsGeneric.nim
@@ -11,6 +11,40 @@ import ../../Utils/utils
 proc getTypeDict*(obj: PyObject|PyObjectObj): PyDictObject = 
   PyDictObject(obj.pyType.dict)
 
+type
+  PyStackRef* = PyObject
+  PyCStackRef* = object
+    `ref`: PyStackRef  ## Nim ref can be used as stackref
+const PyStackRef_NULL* = default PyStackRef
+template PyThreadState_PushCStackRef*(c) = discard
+proc pushedCStackRef*(): PyCStackRef = discard
+template asPyObjectBorrow*(r: PyStackRef): lent PyObject = r  ## PyStackRef_AsPyObjectBorrow
+template asPyObjectSteal*(r: PyStackRef): PyObject = r  ## PyStackRef_AsPyObjectSteal
+proc isNull*(self: PyStackRef): bool =
+  ## PyStackRef_IsNull
+  self.isNil
+
+proc PyStackRef_FromPyObjectSteal*(o: PyObject): PyStackRef = o
+
+proc find_name_in_mro(tp: PyTypeObject, name: PyStrObject, error: var int): PyObject =
+  for base in tp.iterMro:
+    let dicto = base.dict
+    assert not dicto.isNil and dicto.ofPyDictObject
+    let dict = PyDictObject dicto
+    result = dict.getOptionalItem(name)
+    if not result.isNil:
+      return
+  error = 0
+
+proc PyType_LookupStackRefAndVersion*(tp: PyTypeObject, name: PyStrObject, o: var PyObject): uint{.discardable.} =
+  ## `_PyType_LookupStackRefAndVersion`
+  var error: int
+  let res = find_name_in_mro(tp, name, error)
+  if error != 0:
+    o = PyStackRef_NULL
+    return
+  o = PyStackRef_FromPyObjectSteal res
+
 
 proc PyObject_GenericGetAttrWithDict*(self: PyObject; name: PyStrObject|PyObject,
     typeDict: PyDictObject = self.getTypeDict, supress: static[bool] = false): PyObject{.pyCFuncPragma.} =
@@ -21,15 +55,25 @@ proc PyObject_GenericGetAttrWithDict*(self: PyObject; name: PyStrObject|PyObject
   when supress:
     template SupressAE = return nil
   var descr: PyObject
-  typeDict.withValue(name, value):
-    descr = value[]
+
+  let tp = self.pyType
+  var cref = pushedCStackRef()
+  PyType_LookupStackRefAndVersion(tp, name, cref.`ref`)
+  descr = asPyObjectBorrow cref.`ref`
+
+  template tryDescr =
     let descrGet = descr.pyType.magicMethods.get
     if not descrGet.isNil:
-      result = descr.descrGet(self)
+      result = descrGet(descr, self)
       when supress:
         if result.isExceptionOf Attribute:
           SupressAE
-      else: return 
+      else: return
+  if not descr.isNil: tryDescr
+
+  typeDict.withValue(name, value):
+    descr = value[]
+    tryDescr
 
   if self.hasDict:
     let instDict = PyDictObject(self.getDict)
