78bfb2e2bc28be52ec9aa1e45e56f8d71f33783d
litlighilit <litlighilit@foxmail.com>
2025-09-11 21:07:19 +0800

    feat(builtins): reversed (not reg yet)

--- Objects/enumobject.nim
+++ Objects/enumobject.nim
@@ -10,12 +10,18 @@ import ./[
 ]
 import ./numobjects/intobject
 import ./abstract/[
-  iter,
+  iter, sequence,
 ]
-import ../Python/getargs
+import ../Python/[
+  getargs, call,
+]
 import ../Include/cpython/[
   pyatomic, critical_section,
 ]
+import ../Include/internal/[
+  pycore_global_strings,
+]
+import ./typeobject/apis/attrs
 
 declarePyType Enumerate(mutable):
   index: int
@@ -127,4 +133,77 @@ implEnumerateMagic iternext: self.next()
 implEnumerateMagic iter: self
 
 
+
+# Reversed object ***********
+
+declarePyType Reversed(mutable):
+  index: int
+  sequ: PyObject
+
+type reversedobject = PyReversedObject
+
+template noRev =
+  return newTypeError newPyStr &"'{sequ.typeName:.200s}' object is not reversible"
+proc newPyReversed*(sequ: PyObject; typ=pyReversedObjectType): PyObject =
+  let reversed_meth = PyObject_LookupSpecial(sequ, pyDUId reversed)
+  if reversed_meth.isPyNone: noRev
+  if not reversed_meth.isNil:
+    if reversed_meth.isThrownException:
+      return reversed_meth
+    return call reversed_meth
+
+  # is nil. not found
+  if not PySequence_Check(sequ):
+    noRev
+  var n: int
+  retIfExc PySequence_Size(sequ, n)
+  assert n != -1
+  let roE = typ.tp_alloc(typ, 0)
+  retIfExc roE
+  let ro = reversedobject roE
+  ro.index = n-1
+  ro.sequ = sequ
+  return ro
+
+using ro: reversedobject
+proc next*(ro): PyObject =
+  ## reversed_next
+  let index = Py_atomic_load_relaxed(FT ro.index)
+  if index >= 0:
+    let item = PySequence_GetItem(ro.sequ, index)
+    if item.isThrownException:
+      if not (
+        item.ofPyIndexErrorObject or item.ofPyStopIterErrorObject
+      ): return item
+      else:
+        discard item
+    else:
+      Py_atomic_store_relaxed(FT ro.index, index-1)
+      return item
+  Py_atomic_store_relaxed(FT ro.index, -1)
+  newStopIterError()
+
+proc getLen*(ro; res: var int): PyBaseErrorObject =
+  template ret(i: int) =
+    res = i
+    return nil
+  let index = Py_atomic_load_relaxed(FT ro.index)
+  if index == -1:
+    ret 0
+  assert not ro.sequ.isNil
+  var seqsize: int
+  retIfExc PySequence_Size(ro.sequ, seqsize)
+  let position = index + 1
+  ret if seqsize < position: 0 else: position
+
+implReversedMagic New(typ: PyTypeObject, x: PyObject):
+  newPyReversed(x, typ)
+
+implReversedMagic len:
+  var res: int
+  retIfExc self.getLen(res)
+  result = newPyInt res
+
+implReversedMagic iter: self
+implReversedMagic iternext: self.next()
 
