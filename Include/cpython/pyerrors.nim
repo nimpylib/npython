064c003648887674b7ee0f5a5ec0beab616880bc
litlighilit <litlighilit@foxmail.com>
2025-09-11 12:05:03 +0800

    api(inner): PyErr_BadInternalCall

--- Include/cpython/pyerrors.nim
+++ Include/cpython/pyerrors.nim
@@ -1,4 +1,6 @@
 
+import std/strformat
+import ../../Objects/[stringobject, exceptions]
 import ../../Utils/getfuncname
 import ./pyerrors_fatal
 
@@ -11,3 +13,9 @@ proc Py_FatalErrorFunc*(funcname: string, message: string) = Py_FatalErrorFunc(f
 template Py_FatalError*(message) =
   bind instantiationFuncname
   Py_FatalErrorFunc(instantiationFuncname(), message)
+
+proc PyErr_BadInternalCall*(filename: string, lineno: int): PySystemErrorObject =
+  return newSystemError newPyStr &"{filename}:{lineno}: bad argument to internal function"
+template PyErr_BadInternalCall*: PyBaseErrorObject =
+  let tup = instantiationInfo()
+  PyErr_BadInternalCall(tup.filename, tup.line)
