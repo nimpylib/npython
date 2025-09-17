36a4e30d63caab01035a797749f9fa77f66577dd
litlighilit <litlighilit@foxmail.com>
2025-09-17 15:09:38 +0800

    feat(getargs): toval sup bool, string

--- Python/getargs/tovals.nim
+++ Python/getargs/tovals.nim
@@ -1,13 +1,26 @@
 
+import std/strformat
 import ./tovalsBase
 export tovalsBase
 import ../../Objects/[
   pyobject,
   exceptions,
   numobjects,
+  stringobject,
+  boolobjectImpl,
 ]
 
 genToValGeneric(int, Ssize_t, Number)
 genToValGeneric(float, double, Float)
 
+proc converterr(expected: string, arg: PyObject): string =
+  fmt"must be {expected:.50s}, not {arg.typeName:.50s}"
 
+proc `handle %s`(x: PyObject, res: var string): PyBaseErrorObject =
+  if x.ofPyStrObject:
+    res = PyStrObject(x).asUTF8
+    return
+  newTypeError newPyAscii converterr("str", x)
+genToVal string, `handle %s`
+
+genToVal bool, PyObject_IsTrue
