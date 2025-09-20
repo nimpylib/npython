fcb5116aa6663ec6ec6e8f9ac9bbdcb6750aba51
litlighilit <litlighilit@foxmail.com>
2025-09-20 13:37:15 +0800

    feat(builtins): max, min

--- Python/bltinmodule/iterops.nim
+++ Python/bltinmodule/iterops.nim
@@ -10,12 +10,16 @@ import ../../Objects/[
   exceptions,
   noneobject,
   stringobject,
+  pyobject_apis/compare,
 ]
 import ../../Objects/listobject/sort
 import ../../Objects/abstract/[
+  iter,
   sequence/list,
 ]
+import ../call
 import ../getargs/[tovals,]
+import ./utils
 
 
 proc sorted*(sequ: PyObject, key: PyObject = pyNone, reversed = false): PyObject{.bltin_clinicGen.} =
@@ -25,5 +29,94 @@ proc sorted*(sequ: PyObject, key: PyObject = pyNone, reversed = false): PyObject
   retIfExc newlist.sort(key, reversed)
   return newlist
 
+
+proc min_max(args: openArray[PyObject], keyfunc, defaultval: PyObject, op: PyCompareOp, name: string): PyObject =
+  var nargs = args.len
+  if nargs == 0:
+    return newTypeError newPyAscii name&" expected at least 1 argument, got 0"
+
+  # (kwnames != NULL && !_PyArg_ParseStackAndKeywords(args + nargs, 0, kwnames, _parser, &keyfunc, &defaultval)):
+
+  let positional = nargs > 1 # False iff nargs == 1
+  if positional and not defaultval.isNil:
+    return newTypeError newPyAscii(
+                    "Cannot specify a default for "&name&"() with multiple "&
+                    "positional arguments")
+
+  var it: PyObject
+  if not positional:
+    it = PyObject_GetIter(args[0])
+    retIfExc it
+
+  var keyfunc = keyfunc
+  if keyfunc.isPyNone: keyfunc = nil
+
+  var
+    item,
+      maxitem, # the result
+      maxval: PyObject  # the value associated with the result
+    i = 0
+  while true:
+    if positional:
+      if nargs <= 0:
+        nargs.dec
+        break
+      nargs.dec
+      item = args[i]
+      i.inc
+    else:
+      let res = PyIter_NextItem(it, item)
+      case res
+      of Error: return item
+      of Missing:
+        break
+      of Get: discard
+
+    # get the value from the key function
+    var val: PyObject
+    if not keyfunc.isNil:
+      val = keyfunc.call(item)
+      retIfExc val
+    # no key function; the value is the item
+    else:
+      val = item
+
+    # maximum value and item are unset; set them
+    if maxval.isNil:
+      maxitem = item
+      maxval = val
+    # maximum value and item are set; update them as necessary
+    else:
+      var resb: bool
+      let res = PyObject_RichCompareBool(val, maxval, op, resb)
+      retIfExc res
+      if resb:
+        maxval = val
+        maxitem = item
+
+  if maxval.isNil:
+    assert maxitem.isNil
+    if not defaultval.isNil:
+      maxitem = defaultval
+    else:
+      return newValueError newPyAscii name &
+                      "() iterable argument is empty"
+  return maxitem
+
+template gen_min_max(f, op){.dirty.} =
+  proc `builtin f`*(args: varargs[PyObject], kwargs: PyObject): PyObject{.pyCFuncPragma.} =
+    const name = astToStr(f)
+    var key, default: PyObject
+    retIfExc PyArg_UnpackKeywordsTo(name, PyDictObject kwargs, key, default)
+    min_max(args, key, default, op, name)
+
+# AC: cannot convert yet, waiting for *args support
+#proc min*(args: varargs[PyObject], keyfunc: PyObject = nil, defaultval: PyObject = nil): PyObject {.bltin_clinicGen.} =
+gen_min_max min, Py_LT
+gen_min_max max, Py_GT
+
 template register_iterops* =
-  registerBltinFunction "sorted", builtin_sorted
+  bind regfunc
+  regfunc sorted
+  regfunc min
+  regfunc max
