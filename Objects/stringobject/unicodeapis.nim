5a640a3369a189a7246e7bb719940b67137ccb77
litlighilit <litlighilit@foxmail.com>
2025-09-20 14:58:27 +0800

    feat(api/c): PyUnicode_FromOrdinal

---
+++ Objects/stringobject/unicodeapis.nim
@@ -0,0 +1,22 @@
+
+include /common_h
+proc unicode_char(c: uint32): PyObject =
+  assert c <= MAX_UNICODE
+  newPyStr if c < 256: newAsciiUnicodeVariant cast[char](c)
+  else: newUnicodeUnicodeVariant @[cast[Rune](c)]
+
+template retChrOutOfRange =
+  return newValueError newPyAscii"chr() arg not in range(0x110000)"
+template chkUpperBound =
+  if ordinal > MAX_UNICODE: retChrOutOfRange
+proc PyUnicode_FromOrdinal*(ordinal: uint32): PyObject =
+  chkUpperBound
+  unicode_char ordinal
+
+proc PyUnicode_FromOrdinal*(ordinal: int32): PyObject =
+  if ordinal < 0: retChrOutOfRange
+  PyUnicode_FromOrdinal cast[uint32](ordinal)
+
+proc PyUnicode_FromOrdinal*(ordinal: int): PyObject =
+  chkUpperBound
+  unicode_char cast[uint32](ordinal)
--- Objects/stringobjectImpl.nim
+++ Objects/stringobjectImpl.nim
@@ -3,6 +3,6 @@ import ./stringobject
 import ../Utils/trans_imp
 export stringobject
 impExp stringobject,
-  utf8apis, internal, meth
+  utf8apis, internal, meth, unicodeapis
 # XXX: import codec causes rec-dep
 
