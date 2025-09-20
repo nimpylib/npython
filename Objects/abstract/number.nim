1727dee9c48b083063f07bba5724fd04d709d756
litlighilit <litlighilit@foxmail.com>
2025-09-20 15:15:38 +0800

    feat(api/c): PyLong_AsLongAndOverflow

--- Objects/abstract/number.nim
+++ Objects/abstract/number.nim
@@ -6,7 +6,18 @@ import ../[
   stringobject,
   numobjects,
 ]
+import ../../Include/cpython/pyerrors
 export PyNumber_Index, PyNumber_AsSsize_t, PyNumber_AsClampedSsize_t
+
+proc PyLong_AsLongAndOverflow*(vv: PyObject, overflow: var IntSign, res: var int): PyBaseErrorObject =
+  if vv.isNil: return PyErr_BadInternalCall()
+  res = PyIntObject(
+    if vv.ofPyIntObject: vv
+    else:
+      let ret = PyNumber_Index(vv)
+      retIfExc ret
+      ret
+  ).toInt overflow
 
 template genBOp(op){.dirty.} =
   proc `PyNumber op`*(v, w: PyObject): PyObject = v.callMagic(op, w)
