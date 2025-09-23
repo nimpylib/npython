b2f3a1a666d66f3b972f2043691b49171ff1d122
litlighilit <litlighilit@foxmail.com>
2025-09-23 15:25:31 +0800

    feat: PY_VERSION_HEX

--- Python/getversion.nim
+++ Python/getversion.nim
@@ -11,3 +11,20 @@ proc Py_GetCompiler*: string =
 proc Py_GetVersion*: string =
   &"{Version:.80} ({Py_GetBuildInfo():.80}) {Py_GetCompiler():.80}"
 
+
+template Py_PACK_FULL_VERSION(X, Y, Z, LEVEL, SERIAL): untyped = (
+    ((X and 0xff) shl 24) or
+    ((Y and 0xff) shl 16) or
+    ((Z and 0xff) shl 8)  or 
+    ((LEVEL and 0xf) shl 4) or
+    ((SERIAL and 0xf) shl 0))
+
+const PY_VERSION_HEX* =
+  #[ Version as a single 4-byte hex number, e.g. 0x010502B2 == 1.5.2b2.
+   Use this for numeric comparisons, e.g. #if PY_VERSION_HEX >= ... ]#
+  Py_PACK_FULL_VERSION(
+    PY_MAJOR,
+    PY_MINOR,
+    PyPatch,
+    PY_RELEASE_LEVEL,
+    PY_RELEASE_SERIAL)
--- Python/versionInfo.nim
+++ Python/versionInfo.nim
@@ -8,6 +8,10 @@ const
   PyMinor*{.intdefine.} = 13
   PyPatch*{.intdefine.} = 0
 
+  PyReleaseLevel*{.intdefine.} = 0xA
+  PyReleaseLevelStr*{.strdefine.} = "alpha"
+  PY_RELEASE_SERIAL*{.intdefine.} = 0
+
 const sep = '.'
 template asVersion(major, minor, patch: int): string =
   $major & sep & $minor & sep & $patch
