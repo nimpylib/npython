a681fe2f1e7b9a3e0977d8e34f01eb95b68ffe8f
litlighilit <litlighilit@foxmail.com>
2025-10-13 05:52:10 +0800

    feat(api/c): abstract: PyObject_CallMethodObjArgs as callMethodArgs

--- Objects/abstract/call.nim
+++ Objects/abstract/call.nim
@@ -3,8 +3,19 @@ import ../[pyobjectBase, stringobject]
 
 import ../../Python/call
 
+proc callMethod*(self: PyObject, name: PyStrObject): PyObject =
+  ## `PyObject_CallMethodNoArg`
+  vectorcallMethod(name, [self])
+
 proc callMethod*(self: PyObject, name: PyStrObject, arg: PyObject): PyObject =
   ## `PyObject_CallMethodOneArg`
   assert not arg.isNil
   vectorcallMethod(name, [self, arg])
   
+proc callMethodArgs*(self: PyObject, name: PyStrObject, args: varargs[PyObject]): PyObject =
+  ## `PyObject_CallMethodObjArgs`
+  var s = newSeq[PyObject](args.len+1)
+  s[0] = self
+  for i in 1..args.len:
+    s[i] = args[i-1]
+  vectorcallMethod(name, s)
