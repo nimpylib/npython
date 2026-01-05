26732c7c2391a06d3439463b3fadc79004e6d421
litlighilit <litlighilit@foxmail.com>
2026-01-06 03:18:09 +0800

    fix(py): `frame.__repr__`; frame.f_back never None but AttributeError

--- Objects/frameobject.nim
+++ Objects/frameobject.nim
@@ -1,16 +1,18 @@
 
-
+import std/strformat
 import pyobject
 
 import codeobject
 import dictobject
 import cellobject
-
+import stringobject
+import ./pyobject_apis/strings
 
 declarePyType Frame():
-  # currently not used?
-  back{.member"f_back", readonly.}: PyFrameObject
+  #TODO:frame see CPython's frame.c:take_ownership and frameobject.c:PyFrame_GetBack
+  back{.member"f_back", readonly, nil2none.}: PyFrameObject
   code{.member"f_code", readonly.}: PyCodeObject
+  lineno: int = -1
   # dicts and sequences for variable lookup
   # locals not used for now
   # locals*: PyDictObject
@@ -19,6 +21,32 @@ declarePyType Frame():
   fastLocals: seq[PyObject]
   cellVars: seq[PyCellObject]
 
+proc PyUnstable_InterpreterFrame_GetLine(f: PyFrameObject): int =
+  #TODO:PyFrame_GetLineNumber
+  # NOTE: there're also another place storing line number info: exceptionsImpl.nim:newTraceback
+  result = f.code.lineNos[0]
+
+proc getLineNumber*(f: PyFrameObject): int =
+  ## `PyFrame_GetLineNumber`
+  result = f.lineno
+  if result == -1:
+    f.lineno = PyUnstable_InterpreterFrame_GetLine(f)
+    if f.lineno < 0:
+      f.lineno = 0
+  if f.lineno > 0:
+    return f.lineno
+  else:
+    return PyUnstable_InterpreterFrame_GetLine(f)
+
+method `$`*(f: PyFrameObject): string{.raises: [].} =
+  let lineno = f.getLineNumber()
+  let code = f.code
+  let fnObj = PyObjectRepr(code.fileName)
+  assert fnObj.ofPyStrObject
+  let fn = $PyStrObject(fnObj).str
+  return &"<frame at {f.idStr}, file {fn}, line {lineno}, code {code.codeName}>"
+
+implFrameMagic repr: newPyStr $self
 
 # initialized in neval.nim
 proc newPyFrame*: PyFrameObject = 
