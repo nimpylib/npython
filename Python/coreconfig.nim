b41217e3fe004e55e3eab1756116160516ecc94e
litlighilit <litlighilit@foxmail.com>
2026-03-25 12:17:08 +0800

    feat(inner): PyConfig.import_time

--- Python/coreconfig.nim
+++ Python/coreconfig.nim
@@ -2,6 +2,12 @@
 import ../Include/internal/str
 
 type
+
+  PyImportTimePerf*{.pure.} = enum
+    Unknown = -1
+    Off = 0
+    PrintOnlyUnLoaded = 1
+    PrintAlsoLoaded = 2 ## print an import time entry even if an imported module has already been loaded.
   PyConfig* = object
     run_command*,
       run_module*,
@@ -17,6 +23,7 @@ type
     interactive*: bool
     site_import*: bool  ## if `import site` (no `-S` given)
     inspect*: bool
+    import_time*: PyImportTimePerf = Unknown
 
 var pyConfig* = PyConfig(site_import: true)
 
