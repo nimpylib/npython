b6d99b758044eb0f0b04249ec57d5c74158e335a
litlighilit <litlighilit@foxmail.com>
2026-06-14 01:27:01 +0800

    feat(Lib): math (not all)

---
+++ npython/builtinModules/math.nim
@@ -0,0 +1,120 @@
+
+import pkg/pymath
+
+import ../Objects/[
+  pyobject,
+  bltcommon,
+  exceptions,
+  boolobject,
+  moduleobjectImpl,
+  stringobject,
+  numobjects,
+  tupleobjectImpl,
+]
+import ../Python/getargs/[tovals,
+  paramsMeta,
+  dispatch,
+]
+
+const mathModuleName* = "math"
+declarePyType MathModule(base(Module)): discard
+proc PyInit_math*: PyObject = PyModule_CreateInitialized(math)
+
+template gen_const(nam) {.dirty.} =
+  genProperty MathModule, astToStr(nam), nam: newPyFloat(pymath.nam)
+
+gen_const pi
+gen_const e
+gen_const tau
+gen_const inf
+gen_const nan
+
+template gen_func1(nam) {.dirty.} =
+  implMathModuleMethod nam(x: float): newPyFloat(pymath.nam x)
+template gen_fun2i(nam) {.dirty.} =
+  implMathModuleMethod nam(x: float, i: int): newPyFloat(pymath.nam(x, i))
+
+template orRetValErr(pyexp): PyObject =
+    try: pyexp
+    except ValueError as e: return newValueError newPyAscii e.msg
+template gen_funci(nam) {.dirty.} =
+  implMathModuleMethod nam(x: float):
+    newPyFloat(float pymath.nam x).orRetValErr
+template gen_func2(nam) {.dirty.} =
+  implMathModuleMethod nam(x: float, y: float): newPyFloat(pymath.nam(x, y))
+
+template toPyObject(i: int): PyObject = newPyInt i
+template toPyObject(i: float): PyObject = newPyFloat i
+
+template gen_funct(nam) {.dirty.} =
+  implMathModuleMethod nam(x: float):
+    let t = pymath.nam(x)
+    newPyTuple([t[0].toPyObject, t[1].toPyObject])
+
+template gen_funcb(nam) {.dirty.} =
+  implMathModuleMethod nam(x: float): newPyBool(pymath.nam(x))
+
+proc isclose(self: PyMathModuleObject;
+        x: float, y: float,
+        rel_tol{.startKwOnly.}: float = 1e-09, abs_tol: float = 0.0
+    ): PyBoolObject{.clinicGenMethodRaises(MathModule, [ValueError]).} =
+  newPyBool(pymath.isclose(x, y, rel_tol, abs_tol))
+
+gen_func1 acos
+gen_func1 acosh
+gen_func1 asin
+gen_func1 asinh
+gen_func1 atan
+gen_func2 atan2
+gen_func1 atanh
+gen_func1 cbrt
+gen_funci ceil
+#gen_funi2 comb
+gen_func2 copysign
+gen_func1 cos
+gen_func1 cosh
+gen_func1 degrees
+#gen_funp2 dist
+gen_func1 erf
+gen_func1 erfc
+gen_func1 exp
+#gen_func1 exp2
+gen_func1 expm1
+gen_func1 fabs
+#gen_func1 factorial
+gen_funci floor
+gen_func2 fmod
+#gen_func3 fma
+gen_funct frexp
+#gen_func1 fsum
+gen_func1 gamma
+#gen_funiN gcd
+#gen_funiN hypot
+#gen_fubfx isclose #done above
+gen_funcb isfinite
+gen_funcb isinf
+gen_funcb isnan
+#gen_funci isqrt  #TODO:intobject
+#gen_funiN lcm
+gen_fun2i ldexp
+gen_func1 lgamma
+gen_func2 log
+gen_func1 log10
+gen_func1 log1p
+gen_func1 log2
+gen_funct modf
+gen_func2 nextafter
+#gen_func2 perm
+gen_func2 pow
+#gen_funix prod
+gen_func1 radians
+gen_func2 remainder
+gen_func1 sin
+gen_func1 sinh
+gen_func1 sqrt
+#gen_funcx sumprod
+gen_func1 tan
+gen_func1 tanh
+gen_funci trunc
+gen_func1 ulp
+
