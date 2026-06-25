b35b69a13ccddb6b01f3f5e16e92442f28909a64
litlighilit <litlighilit@foxmail.com>
2026-06-26 02:58:03 +0800

    fixup(c8fb1b33): `feat(Lib/os): sup PathLike`

--- npython/builtinModules/private/dispatch.nim
+++ npython/builtinModules/private/dispatch.nim
@@ -11,22 +11,25 @@ proc normalizePathLikeParamForClinic(p: NimNode) =
     for i in 0..<p.len - 2:
       p[i] = ident p[i].strVal
       # prevent `Error: cannot use symbol of kind 'param' as a 'var'`
-    if p[1].eqIdent"PathLike":
-      p[1] = bindSym"PyPathStr"
+    let p1 = p[^2]
+    if p1.eqIdent"PathLike" or (
+        p1.kind == nnkBracketExpr and p1[0].eqIdent"PathLike"):
+      p[^2] = bindSym"PyPathStr"
 
 proc pathLikeProcDefForClinic(prc: NimNode): NimNode =
   let emptyn = newEmptyNode()
+  let params = prc.params.copyNimTree
+  for i in 1..<params.len:
+    params[i].normalizePathLikeParamForClinic
   result = nnkProcDef.newTree(
     prc.name,
     emptyn,
     emptyn,
-    prc.params.copyNimTree,
+    params,
     emptyn,
     emptyn,
     emptyn,
   )
-  for i in 1..<result.params.len:
-    result.params[i].normalizePathLikeParamForClinic
 
 template genClinicGen*(os; DefExceptions: untyped = []) {.dirty.} =
   bind clinicGenStaticMethodOfKindImpl, getProcDefFromSpec
