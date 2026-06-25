206cc973eec4af179ae3dc7f9e8ac22d5cda5bbb
litlighilit <litlighilit@foxmail.com>
2026-06-26 00:05:51 +0800

    fix(bool): int(bool) always returned 0

--- npython/Objects/boolobject.nim
+++ npython/Objects/boolobject.nim
@@ -1,16 +1,19 @@
-import pyobject
+
+import ./pyobject
 import ./numobjects/intobject/decl
+import pkg/intobject
 
 declarePyType Bool(tpToken, typeName("bool"), base(Int), singleton):
   b: bool
 
-proc newPyBoolInner(b: bool): PyBoolObject = 
+proc newPyBoolInner(b: bool, v: IntObject): PyBoolObject = 
   result = newPyBoolSimple()
   result.b = b
+  result.v = v  # ensure `int(b)` works
 
 
-let pyTrueObj* = newPyBoolInner(true)  ## singleton
-let pyFalseObj* = newPyBoolInner(false)  ## singleton
+let pyTrueObj* =  newPyBoolInner(true, intOne)  ## singleton
+let pyFalseObj* = newPyBoolInner(false,intZero)  ## singleton
 
 proc newPyBool*(b: bool): PyBoolObject =
   if b: pyTrueObj
