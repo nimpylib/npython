42193b80ac32134419dd17a4fa3c04837fa293cb
litlighilit <litlighilit@foxmail.com>
2026-07-29 23:07:12 +0800

    bypass(NIM-BUG): libffi not compile under mingw

--- npython/builtinModules/ctypes/ffi/ffi.nim
+++ npython/builtinModules/ctypes/ffi/ffi.nim
@@ -1,6 +1,13 @@
 
 import pkg/handy_sugars/trans_imp
-when defined(js):
+when defined(js) or defined(windows):
+  # Under windows: tested on 7/29/26,
+  #  C compiler errors:
+  #[
+  error: 'FFI_SYSV' undeclared
+  error: implicit declaration of function 'ffi_call_SYSV'
+  
+  ]#
   impExpCwd ffi, [ffi_shim]
 else:
   impExpCwd ffi, [ffi_full_ugly]
