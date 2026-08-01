021404fabe885fe42e7e464816372583df82c478
litlighilit <litlighilit@foxmail.com>
2026-08-02 02:04:12 +0800

    fixup! fix(Lib/ctypes): not compile under Windows due to pkg/libffi

--- npython/builtinModules/ctypes/ffi/ffi/consts.nim
+++ npython/builtinModules/ctypes/ffi/ffi/consts.nim
@@ -1,4 +1,4 @@
-const NoFFI* = defined(js) or defined(windows)
+const NoFFI* = defined(js) or defined(windows) or defined(macosx)
   # Under windows: tested on 7/29/26,
   #  C compiler errors:
   #[
@@ -6,4 +6,8 @@ const NoFFI* = defined(js) or defined(windows)
   error: implicit declaration of function 'ffi_call_SYSV'
   
   ]#
-
+  # under macos: tested on 8/1/26
+  # Runtime errors:
+  #[
+  could not import: ffi_type_longdouble
+  ]#
