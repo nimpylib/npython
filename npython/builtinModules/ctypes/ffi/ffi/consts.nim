81f8c8151267c59b81dee00c4e0abadfbcf40ca1
litlighilit <litlighilit@foxmail.com>
2026-08-02 02:45:08 +0800

    test(Lib/ctypes): not compile under MacOS due to pkg/libffi

--- npython/builtinModules/ctypes/ffi/ffi/consts.nim
+++ npython/builtinModules/ctypes/ffi/ffi/consts.nim
@@ -1,13 +1,7 @@
-const NoFFI* = defined(js) or defined(windows) or defined(macosx)
+const NoFFI* = defined(js) or defined(windows)
   # Under windows: tested on 7/29/26,
   #  C compiler errors:
   #[
   error: 'FFI_SYSV' undeclared
   error: implicit declaration of function 'ffi_call_SYSV'
-  
-  ]#
-  # under macos: tested on 8/1/26
-  # Runtime errors:
-  #[
-  could not import: ffi_type_longdouble
   ]#
